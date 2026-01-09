b686df58bb36873a3d81f30b7be13da84be93f3b
dem._.mo <wikham2550@icloud.com>
2026-01-09 16:38:29 +0700

    Update supabaseAdmin.ts

--- src/lib/supabaseAdmin.ts
+++ src/lib/supabaseAdmin.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseAdmin = createClient(
-  process.env.SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY!
+const supabase = createClient(
+  import.meta.env.PUBLIC_SUPABASE_URL,  // client-safe
+  import.meta.env.PUBLIC_SUPABASE_ANON_KEY
 );
