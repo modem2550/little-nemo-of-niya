fbde0a3b78a04f74d000498e76b8fa57682456dc
dem._.mo <dem._.mo@dem-mos-MacBook-Air-4.local>
2026-01-21 09:42:28 +0700

    fix: astro config and SLW

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,35 +1,16 @@
-import { defineConfig, envField } from 'astro/config';
-import vercel from '@astrojs/vercel/server';
+import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
 
-  env: {
-    schema: {
-      SUPABASE_URL: envField.string({
-        context: 'server',
-        access: 'secret',
-      }),
-      SUPABASE_ANON_KEY: envField.string({
-        context: 'server',
-        access: 'secret',
-      }),
-      ADMIN_TOKEN: envField.string({
-        context: 'server',
-        access: 'secret',
-        optional: true,
-      }),
-    },
-  },
-
   image: {
     domains: ['kqfnhyaktxgulhitdvqq.supabase.co'],
   },
 
-  vite: {
-    ssr: {
-      noExternal: ['clsx'],
-    },
+  redirects: {
+    '/events-images/[...slug]':
+      'https://kqfnhyaktxgulhitdvqq.supabase.co/storage/v1/object/public/event-images/events/[...slug]',
   },
 });
